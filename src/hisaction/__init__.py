8a3075d13dfa437936abbdb6cd3fbdc0d78e9b7e
Taylor Monacelli <taylormonacelli@gmail.com>
2024-06-05 10:04:56 -0700

    Boilerplate

---
+++ src/hisaction/__init__.py
@@ -0,0 +1,28 @@
+import sys
+
+from . import cli, main2
+
+__project_name__ = "hisaction"
+
+
+def main() -> int:
+    parser = cli.get_args_parser(__project_name__)
+    args = parser.parse_args()
+
+    if not args.command:
+        out = main2.render_template("extended.j2")
+        sys.stdout.write(out)
+
+    if args.command == "config":
+        if args.subcommand:
+            args.func(args)
+        else:
+            print("Config command")
+            if args.verbose:
+                print("Verbose mode enabled")
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/hisaction/__main__.py
@@ -0,0 +1,5 @@
+import sys
+
+import hisaction
+
+sys.exit(hisaction.main())
